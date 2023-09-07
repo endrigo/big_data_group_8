c77b0b215bd9d859da195acf95014416b778176c
Endrigo Rivas <endrigo.rivas@gmail.com>
2023-09-07 08:55:56 -0400

    feat: add scrapping of all the document XML

--- src/scrapping.py
+++ src/scrapping.py
@@ -23,96 +23,189 @@ def scrape_quotes():
         quote['lastName'] = getValue(name.lastName)
         quote['sdnType'] = getValue(name.sdnType)
 
-        quotes.append(quote)
-        continue
-
         programList = name.find_all('programList')
         if programList is None :
             continue
         else:
+            programs = []    
+
             for program in programList: #.stripped_strings):
-                program = getValue(program)
-                print(f'>> {program}')
+                # program = getValue(program)
+                # print(f'>> {program}')
+                programs.append({
+                    "program": getValue(program)
+                    })
+            
+            quote['programs'] = programs
+
+        #     print(f'>> {quote["programs"]}')
+        
+        # quotes.append(quote)
+        # continue
+
 
         akaList = name.find_all('akaList')
         if akaList is None :
             continue
         else:
+
+            akas = []
+
             for aka in akaList:
 
                 akaUid = getValue(aka.uid)
                 akaType = getValue(aka.type)
                 akaCategory = getValue(aka.category)
                 akaLastName = getValue(aka.lastName)
-                print(f'>>> {akaUid}')
-                print(f'>>> {akaType}')
-                print(f'>>> {akaCategory}')
-                print(f'>>> {akaLastName}')
+                # print(f'>>> {akaUid}')
+                # print(f'>>> {akaType}')
+                # print(f'>>> {akaCategory}')
+                # print(f'>>> {akaLastName}')
+
+                akas.append({
+                    "uid": akaUid,
+                    "type":  akaType,
+                    "category": akaCategory,
+                    "lastName": akaLastName
+                    })
+
+        quote['akas'] = akas
+
 
         addressList = name.find_all('addressList')
         if addressList is None :
             continue
         else:
+            addressArray = []
+
             for address in addressList:
                 addressUid = getValue(address.uid)
                 addressCity = getValue(address.city)
                 addressCountry = getValue(address.country)
-                print(f'>>>> {addressUid}')
-                print(f'>>>> {addressCity}')
-                print(f'>>>> {addressCountry}')
+                # print(f'>>>> {addressUid}')
+                # print(f'>>>> {addressCity}')
+                # print(f'>>>> {addressCountry}')
+
+                addressArray.append({
+                    "uid": addressUid,
+                    "city":  addressCity,
+                    "country": addressCountry
+                    })
+
+        quote['address'] = addressArray
+
+        #quotes.append(quote)
+        #continue
 
         idsList = name.find_all('idList')
         if idsList is None :
             continue
         else:
+            idsArray = []
+
             for ids in idsList:
                 idListUid = getValue(ids.uid)
                 idListType = getValue(ids.idType)
                 idListNumber = getValue(ids.idNumber)
                 idListCountry = getValue(ids.idCountry)
-                print(f'>>>>> {idListUid}')
-                print(f'>>>>> {idListType}')
-                print(f'>>>>> {idListNumber}')
-                print(f'>>>>> {idListCountry}')
+                # print(f'>>>>> {idListUid}')
+                # print(f'>>>>> {idListType}')
+                # print(f'>>>>> {idListNumber}')
+                # print(f'>>>>> {idListCountry}')
+
+                idsArray.append({
+                    "uid": idListUid,
+                    "idType":  idListType,
+                    "idNumber": idListNumber,
+                    "idCountry": idListCountry
+                    })
+
+        quote['ids'] = idsArray
+
+        #quotes.append(quote)
+        # continue
+
 
         nationalityList = name.find_all('nationalityList')
         if nationalityList is None :
             continue
         else:
+            nationalities = []
+
             for nationality in nationalityList:
                 nationalityUid = getValue(nationality.uid)
                 nationalityCountry = getValue(nationality.country)
                 nationalityMainEntry = getValue(nationality.mainEntry)
-                print(f'>>>>>> {nationalityUid}')
-                print(f'>>>>>> {nationalityCountry}')
-                print(f'>>>>>> {nationalityMainEntry}')
+                # print(f'>>>>>> {nationalityUid}')
+                # print(f'>>>>>> {nationalityCountry}')
+                # print(f'>>>>>> {nationalityMainEntry}')
   
+                nationalities.append({
+                    "uid": nationalityUid,
+                    "country":  nationalityCountry,
+                    "mainEntry": nationalityMainEntry
+                    })
+
+        quote['nationalities'] = nationalities
+
+        #quotes.append(quote)
+        #continue
+
+
         dateOfBirthList = name.find_all('dateOfBirthList')
         if dateOfBirthList is None :
             continue
         else:
+            datesOfBirth = []
+
             for dateOfBirth in dateOfBirthList:
                 dateOfBirthUid = getValue(dateOfBirth.uid)
                 dateOfBirthDate = getValue(dateOfBirth.country)
                 dateOfBirthMainEntry = getValue(dateOfBirth.mainEntry)
-                print(f'>>>>>>> {dateOfBirthUid}')
-                print(f'>>>>>>> {dateOfBirthDate}')
-                print(f'>>>>>>> {dateOfBirthMainEntry}')
+                # print(f'>>>>>>> {dateOfBirthUid}')
+                # print(f'>>>>>>> {dateOfBirthDate}')
+                # print(f'>>>>>>> {dateOfBirthMainEntry}')
 
-        placeOfBirthList = name.find_all('dateOfBirthList')
+                datesOfBirth.append({
+                    "uid": dateOfBirthUid,
+                    "country":  dateOfBirthDate,
+                    "mainEntry": dateOfBirthMainEntry
+                    })
+
+        quote['datesOfBirth'] = datesOfBirth
+
+        #quotes.append(quote)
+        #continue
+
+
+        placeOfBirthList = name.find_all('placeOfBirthList')
         if placeOfBirthList is None :
             continue
         else:
+
+            placesOfBirth = []
+
             for placeOfBirth in placeOfBirthList:
                 placeOfBirthUid = getValue(placeOfBirth.uid)
-                placeOfBirthPlace = getValue(placeOfBirth.country)
+                placeOfBirthPlace = getValue(placeOfBirth.placeOfBirth)
                 placeOfBirthMainEntry = getValue(placeOfBirth.mainEntry)
-                print(f'>>>>>>>> {placeOfBirthUid}')
-                print(f'>>>>>>>> {placeOfBirthPlace}')
-                print(f'>>>>>>>> {placeOfBirthMainEntry}')
+                # print(f'>>>>>>>> {placeOfBirthUid}')
+                # print(f'>>>>>>>> {placeOfBirthPlace}')
+                # print(f'>>>>>>>> {placeOfBirthMainEntry}')
 
+                placesOfBirth.append({
+                    "uid": placeOfBirthUid,
+                    "placeOfBirth":  placeOfBirthPlace,
+                    "mainEntry": placeOfBirthMainEntry
+                    })
 
-        print("-----------")
+        #quote['placesOfBirth'] = placesOfBirth
+
+        #Save all into main Array
+        quotes.append(quote)
+        # continue
+
+        # print("-----------")
 
     return quotes
 
@@ -140,9 +233,11 @@ username = urllib.parse.quote_plus('mongoadmin')
 password = urllib.parse.quote_plus('bdung')
 
 client = pymongo.MongoClient('mongodb://%s:%s@127.0.0.1' % (username, password))
-db = client.db.debts
+db = client.db.ofac
 try:
     db.insert_many(quotes)
     print(f'inserted {len(quotes)} articles')
-except:
-    print('an error occurred quotes were not stored to db')
+# except:
+    # print('an error occurred quotes were not stored to db')
+except Exception as error:
+  print("an error occurred debts were not stored to db:", error)
